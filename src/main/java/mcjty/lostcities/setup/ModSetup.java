13f7c21ff81349c2c7aec2e6691911ea5cc7cd2c
strubium <113206902+strubium@users.noreply.github.com>
2023-10-16 10:27:23 -0500

    Update ModSetup.java

--- src/main/java/mcjty/lostcities/setup/ModSetup.java
+++ src/main/java/mcjty/lostcities/setup/ModSetup.java
@@ -35,7 +35,7 @@ public class ModSetup {
 
     public void preInit(FMLPreInitializationEvent e) {
         logger = e.getModLog();
-        PacketHandler.registerMessages("lostcities");
+        PacketHandler.registerMessages("lostcitiessquared");
 
         setupModCompat();
 
