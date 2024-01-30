11b9e627a1439bb90423496f4381b9bc1cbfb6ff
strubium <113206902+strubium@users.noreply.github.com>
2024-01-30 11:10:21 -0600

    Fix

--- src/main/java/mcjty/lostcities/setup/ModSetup.java
+++ src/main/java/mcjty/lostcities/setup/ModSetup.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
 
-import static mcjty.lostcities.config.MWC_LOOT;
+import static mcjty.lostcities.config.LostCityConfiguration.MWC_LOOT;
 
 public class ModSetup {
 
