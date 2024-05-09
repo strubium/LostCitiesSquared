4a942d71293401ad753cf91fda7ff4268bcd6c15
strubium <113206902+strubium@users.noreply.github.com>
2024-05-09 12:35:05 -0500

    bad idea

--- src/main/java/mcjty/lostcities/dimensions/world/lost/structure/LostStructureWoodlandMansion.java
+++ src/main/java/mcjty/lostcities/dimensions/world/lost/structure/LostStructureWoodlandMansion.java
@@ -28,7 +28,7 @@ public class LostStructureWoodlandMansion extends MapGenStructure {
     }
 
     @Override
-    public String getStructureName() {
+    public static String getStructureName() {
         return "LostMansion";
     }
 
